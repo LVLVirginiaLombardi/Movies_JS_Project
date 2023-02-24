c310df9fe57c5a89991fb993d7e00bee5a5e1e05
Lilian Virginia Lombardi López <virginialombardii@gmail.com>
2023-02-24 20:23:12 -0300

    Changes

--- js/datos.js
+++ js/datos.js
@@ -25,105 +25,105 @@ const DATOS = [
       "numero": 1,
       "tipo": "Digital",
       "precio": "1000",
-      "imagen": "./img/5.jpeg"
+      "imagen": "img/5.jpeg"
     },
     {
       "nombre": "Girl in Pink",
       "numero": 2,
       "tipo": "Digital",
       "precio": "800",
-      "imagen": "./img/4.jpeg"
+      "imagen": "img/4.jpeg"
     }, 
     {
       "nombre": "Girl with white Eyes",
       "numero": 3,
       "tipo": "Digital",
       "precio": "1000",
-      "imagen": "./img/6.jpeg"
+      "imagen": "img/6.jpeg"
     },
     {
       "nombre": "Alien Girl",
       "numero": 4,
       "tipo": "Fibra en Papel Común",
       "precio": "800",
-      "imagen": "./img/10.jpeg"
+      "imagen": "img/10.jpeg"
     },
     {
       "nombre": "Girl in Green",
       "numero": 5,
       "tipo": "Digital",
       "precio": "700",
-      "imagen": "./img/8.jpeg"
+      "imagen": "img/8.jpeg"
     },
     {
       "nombre": "Girl in White",
       "numero": 6,
       "tipo": "Digital",
       "precio": "1000",
-      "imagen": "./img/7.jpeg"
+      "imagen": "img/7.jpeg"
     },
     {
       "nombre": "Muelle",
       "numero": 7,
       "tipo": "Acuarela",
       "precio": "500",
-      "imagen": "./img/2.jpeg"
+      "imagen": "img/2.jpeg"
     },
     {
       "nombre": "Bosque",
       "numero": 8,
       "tipo": "Acuarela",
       "precio": "800",
-      "imagen": "./img/3.jpeg"
+      "imagen": "img/3.jpeg"
     },
     {
       "nombre": "Girl in Cage",
       "numero": 9,
       "tipo": "Acuarela",
       "precio": "1000",
-      "imagen": "./img/1.jpeg"
+      "imagen": "img/1.jpeg"
     },
     {
       "nombre": "Telas",
       "numero": 10,
       "tipo": "Acuarela",
       "precio": "700",
-      "imagen": "./img/acrilico.JPEG"
+      "imagen": "img/acrilico.JPEG"
     },
     {
       "nombre": "Amor ....",
       "numero": 11,
       "tipo": "Acuarela",
       "precio": "1000",
-      "imagen": "./img/body.JPG"
+      "imagen": "img/body.JPG"
     },
     {
       "nombre": "Diablo Girl",
       "numero": 12,
       "tipo": "Acuarela",
       "precio": "1000",
-      "imagen": "./img/11.jpeg"
+      "imagen": "img/11.jpeg"
     },
     {
       "nombre": "Girl Cat Eyes",
       "numero": 13,
       "tipo": "Digital",
       "precio": "1000",
-      "imagen": "./img/photo2.JPG"
+      "imagen": "img/photo2.JPG"
     },
     {
       "nombre": "Girl on Fire",
       "numero": 14,
       "tipo": "Acuarela",
       "precio": "1000",
-      "imagen": "./img/photo_logo.PNG"
+      "imagen": "img/photo_logo.PNG"
     },
     {
       "nombre": "Girl from Water",
       "numero": 15,
       "tipo": "Fibra en Papel Común",
       "precio": "1000",
-      "imagen": "./img/producto.PNG"
+      "imagen": "img/producto.PNG"
     }
 
   
